7aa7ef451e2f67f09cdac48d341e42fcd16cad9a
Alexia Acevedo <alexia.acevedo@knights.ucf.edu>
2022-06-15 20:42:14 -0600

    Created Navigation bar components and styling
    - added sidebar files

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import {
+    Nav, 
+    NavbarContainer, 
+    NavLogo, 
+    MobileIcon,
+    NavMenu, 
+    NavItem,
+    NavLinks
+    } from './NavbarElements'
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/">
+                        Alexia
+                    </NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="skills">Skills</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="projects">Projects</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="contact">Contact</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
